086060e65a4c8dd0657150921ddbbd94f2c072e4
Alvin Leung <alvinleung94@gmail.com>
2015-01-26 17:19:10 -0500

    Initial Commit

---
+++ HSK Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HSK Test
+//
+//  Created by Alvin Leung on 2015-01-26.
+//  Copyright (c) 2015 Alvin Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
